2eeab21a468cb089543272fe26cc049b5ce7d2d9
Muthulakshmi M <muthulakshmi.m@nilaappsindia.com>
2020-05-28 07:20:44 +0530

    Complete initial web app
    Complete initial web app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // styles: ['body { background: red; }'],
+})
+@Component({
+  selector: "body", 
+  styles: ['body { background: red; }'],
+
+  host: {
+    "[style.background-image]":"bodyBackgroundImage()" 
+  },
 })
 export class AppComponent {
   title = 'Welcome to your new beginning!';
+  fullImagePath: string;
+  fullImagePath1: string;
+  gmailiconpath: string;
+  linkediconpath: string;
+  githubiconpath: string;
+  twittericonpath: string;
+
+  constructor() {
+    this.fullImagePath = 'assets/image1.jpg';
+    this.fullImagePath1 = 'assets/image2.jpg';
+    this.gmailiconpath = 'assets/gmailicon.png';
+    this.linkediconpath = 'assets/linkedinicon.png';
+    this.githubiconpath = 'assets/github.png';
+    this.twittericonpath = 'assets/twittericon.png';
+  }
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    // FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
